67463d55d741f762105268222a1d3bd326dd5127
rolandog <rolandog@e8f42418-9959-11dd-a26a-893737b22a74>
2008-11-12 14:00:20 +0000

    Adding fix for rolandog.com

---
+++ trunk/fix.js
@@ -0,0 +1,32 @@
+/** 
+ * fix.js
+ * http://rolandog.com/math-js/
+ *
+ * License: Creative Commons Attribution-Share Alike 3.0 Unported.
+ * http://creativecommons.org/licenses/by-sa/3.0/
+ *
+ * @projectDescription   A fix for displaying stuff on rolandog.com
+ * @author               Rolando Garza rolandog@gmail.com
+ */
+
+"use strict";
+
+/**
+ * Gets a file and calls a function.
+ * @param(Number) a A Number.
+ */
+http.get = function get(url) {
+    var AJAX, e;
+    if (window.XMLHttpRequest) {
+        AJAX = new XMLHttpRequest();
+    } else {
+        AJAX = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    if (AJAX) {
+        AJAX.open("GET", "http://rolandog.com/wp-content/uploads/2008/11/" + url, false);
+        AJAX.send(null);
+        return AJAX.responseText;
+    } else {
+        return false;
+    }
+};
