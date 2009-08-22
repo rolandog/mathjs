0a554bf587416f54c637da56691b869731b5e745
rolandog <rolandog@e8f42418-9959-11dd-a26a-893737b22a74>
2009-08-22 17:54:28 +0000

    Forgot a 'blank' in pEProblems

--- trunk/projectEuler.js
+++ trunk/projectEuler.js
@@ -76,7 +76,7 @@ var http = {
     undefined,
     "What is the first term in the Fibonacci sequence to contain 1000 digits?",
     "A unit fraction contains 1 in the numerator. The decimal representation of the unit fractions with denominators 2 to 10 are given:1/2    =     0.5\n1/3    =     0.(3)\n1/4    =     0.25\n1/5    =     0.2\n1/6    =     0.1(6)\n1/7    =     0.(142857)\n1/8    =     0.125\n1/9    =     0.(1)\n1/10    =     0.1\n\nWhere 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be seen that 1/7 has a 6-digit recurring cycle.\nFind the value of d  1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.",
-    undefined, undefined, undefined, undefined,
+    undefined, undefined, undefined, undefined, undefined,
     undefined, undefined, undefined, undefined, undefined,
     undefined, undefined, undefined, undefined, undefined,
     undefined, undefined, undefined, undefined, undefined,
